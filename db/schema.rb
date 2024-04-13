3f2f9a24a3bc1f9ac9c8ea24f75c506bc3637c3a
yuto1017 <yuto1017.oyama@gmail.com>
2024-04-13 15:34:19 +0900

    Basic認証の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_13_060057) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end

06a538ade17ff97090b3e68d9193477b2ccdc286
yuto1017 <yuto1017.oyama@gmail.com>
2024-04-23 21:35:37 +0900

    ユーザー管理機能の修正#3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,13 @@
 class ItemsController < ApplicationController
 
-  def index
-    @items = Item.all
-  end
+  # 今後実装する予定
+  #def index
+    #@items = Item.all
+  #end
 
-  def new
-    @item = Item.new
-  end
+  # 今後実装する予定
+  #def new
+    #@item = Item.new
+  #end
 
 end

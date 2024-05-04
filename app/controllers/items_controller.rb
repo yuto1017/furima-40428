be8ed977ce403975a049f042d5bd7c1f40269cdb
yuto1017 <yuto1017.oyama@gmail.com>
2024-05-04 14:25:11 +0900

    商品削除機能の修正#1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,8 +47,8 @@ class ItemsController < ApplicationController
   def destroy
     if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 
   private
